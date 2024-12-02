2569eeec7fd8b88ca9b02511a5ed3b5648c585da
revalue <a3344355@gmail.com>
2024-12-02 09:23:12 +0800

    已完成已有API测试，结果已经给到相关开发者

--- testingscript.py
+++ testingscript.py
@@ -36,13 +36,15 @@ def test_student():
     test_add_teacher()
 
 
-with open("./temp_store.txt", "r") as f:
-    course_start=int(f.readline())+15
-    cno_start=int(f.readline())+10
+# with open("./temp_store.txt", "r") as f:
+#     course_start=int(f.readline())+15
+#     cno_start=int(f.readline())+10
 # course_start=185
 # cno_start=50
-with open("./temp_store.txt", "w") as f:
-    f.writelines([str(course_start),'\n',str(cno_start)])
+# with open("./temp_store.txt", "w") as f:
+#     f.writelines([str(course_start),'\n',str(cno_start)])
+course_start=1
+cno_start=1
 
 def test_add_course():
     for i in range(15):
@@ -63,14 +65,23 @@ def test_addhandout2class():
         myDB.add_handout("test_handout_"+str(i),"./"+str(i)+".txt")
     for i in range(2):
     
-        myDB.post_handout(i+cno_start,i+13,"2022000001")#这里hno先设成i+1
-        myDB.post_handout(i+cno_start,i+14,"2022000001")#这里hno先设成i+1
+        myDB.post_handout(i+cno_start,i+1,"2022000001")#这里hno先设成i+1
+        myDB.post_handout(i+cno_start,i+1,"2022000001")#这里hno先设成i+1
 def test_addassignment2class():
     for i in range(5):
         myDB.add_assignment("test_assignment_"+str(i),114514,"测试中文profile","./"+str(i)+".txt")
     for i in range(2):
         myDB.post_assignment(i+1,i+1,"2022000001")#这里hno先设成i+1
         myDB.post_assignment(i+2,i+2,"2022000001")#这里hno先设成i+1
+def test_select_info_home():
+    for i in range(1):
+        result=myDB.select_info_home("202100"+str(i).zfill(4))
+        print("查询学生信息测试result:",result)
+    for i in range(1):
+        result=myDB.select_student_src("202200"+str(i).zfill(4),"test_course_"+str(i))
+        result2=myDB.select_student_work("202100"+str(i).zfill(4),"test_course_"+str(i))
+        print("查询教师信息测试result:",result)
+        print("查询教师信息测试result2:",result2)
     
 def collective_class_func_test():
     #数据库里保存的“文件”都只是文件路径
@@ -78,10 +89,13 @@ def collective_class_func_test():
     test_add_class()
     test_add_student2class()
     test_add_teacher2class()
+
     print("test_addhandout2class")
     test_addhandout2class()
     print("test_addassignment2class")
     test_addassignment2class()
+
+    test_select_info_home()
         
     
  
@@ -96,7 +110,9 @@ def add_course_test():
 #     #csemester 例子：20241
 #     pass
 
-myDB.truncate_tables()
+# myDB.truncate_tables()
+myDB.drop_tables()
+myDB.create_tables()
 test_student()
 
 
